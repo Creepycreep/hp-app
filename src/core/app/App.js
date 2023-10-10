04136cd84f8ea232734e6fa80408b7f61e098bd6
Elizaveta Shevtsova <111qw95@gmail.com>
2023-10-10 23:49:56 +0300

    base styles

--- src/core/app/App.js
+++ src/core/app/App.js
@@ -1,21 +1,40 @@
-import './App.scss';
+import './App.scss'
+
+import Icon from '@mdi/react';
+import { mdiAccountCircleOutline } from '@mdi/js';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Lorem</h1>
+      <h2>Lorem</h2>
+      <h3>Lorem</h3>
+      <h4>Lorem</h4>
+      <h5>Lorem</h5>
+      <h6>Lorem</h6>
+      <p>Lorem</p>
+
+      <button className='button button--filled  button--sm'>
+        <Icon path={mdiAccountCircleOutline} size={'1em'} />
+        <span>Hello</span>
+        <Icon path={mdiAccountCircleOutline} size={'1em'} />
+      </button>
+
+      <button className='button button--filled'>
+        <Icon path={mdiAccountCircleOutline} size={'20px'} />
+        <span>Hello</span>
+        <Icon path={mdiAccountCircleOutline} size={'20px'} />
+      </button>
+
+      <button className='button button--outline button--lg'>
+        <Icon path={mdiAccountCircleOutline} size={'20px'} />
+        <span>Hello</span>
+        <Icon path={mdiAccountCircleOutline} size={'20px'} />
+      </button>
+
+      <input type="text" defaultValue={'Hello'} />
     </div>
   );
 }
